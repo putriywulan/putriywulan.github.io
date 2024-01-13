652273162d307f24f268bdbb771ca446bbcd8c5e
Putri Wulan <putriywulansari@gmail.com>
2024-01-13 18:03:10 +0700

    move to gh page

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js'
+
+var options = {
+  strings: ["I'm a Front-end Developer", "I Solve Problems", "Bringing Designs to Life, Line by Line"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true,
+}
+
+var typed = new Typed('#headline', options)
